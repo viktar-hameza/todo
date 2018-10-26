93e16e12eac9ea2b0dddf13bef2922b58eea520a
viktar hameza <viktor.hameza@gmail.com>
2018-10-26 19:35:14 +0300

    added project

---
+++ src/components/SeachPanel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeachPanel = () => {
+  return (
+    <input placeholder="search" />
+  );
+};
+
+export default SeachPanel;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AppHeader from './components/AppHeader';
+import SeachPanel from './components/SeachPanel';
+import TodoList from './components/TodoList';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+
+  const todoDate = [
+    {label: 'Drink Coffee', important:false},
+    {label: 'Make Awesome App', important:true},
+    {label: 'Have a lunch', important:false}
+  ];
+  const isLoggedIn = false;
+  const loginBox = <span>Login in please</span>;
+  return (
+    <div>
+      { isLoggedIn ? null : loginBox }
+      <AppHeader/>
+      <SeachPanel/>
+      <TodoList todos={todoDate}/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
